931a2f0ae71a38c54dab185d01f6a0ca113fdd9b
Alec Trievel <atrievel@ceiamerica.com>
2020-09-04 17:32:34 -0400

    Setup routing for base components

--- spacex-graphql/src/app/app-routing.module.ts
+++ spacex-graphql/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewSelectorComponent } from './view-selector/view-selector.component';
+import { LaunchesListComponent } from './launches-list/launches-list.component';
+import { ShipsListComponent } from './ships-list/ships-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewSelectorComponent
+  },
+  {
+    path: 'launches',
+    component: LaunchesListComponent
+  },
+  {
+    path: 'ships',
+    component: ShipsListComponent
+  },
+  {
+    path: 'ships/:id',
+    component: ShipsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
