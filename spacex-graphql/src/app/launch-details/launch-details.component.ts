a8b76ec69fd49f543fd8a4c7ec1fde1e903c9c2e
Alec Trievel <atrievel@ceiamerica.com>
2020-09-08 17:02:14 -0400

    Add launch details implementation

--- spacex-graphql/src/app/launch-details/launch-details.component.ts
+++ spacex-graphql/src/app/launch-details/launch-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap, finalize } from 'rxjs/operators';
+import { LaunchDetailsGQL } from '../services/SpacexGraphql.service';
 
 @Component({
   selector: 'app-launch-details',
@@ -7,8 +10,20 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LaunchDetailsComponent implements OnInit {
+  loading = true;
 
-  constructor() { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly launchDetailsService: LaunchDetailsGQL
+  ) {}
+
+  launchDetails$ = this.route.paramMap
+    .pipe(
+      map((params) => params.get('id') as string),
+      switchMap((id) => this.launchDetailsService.fetch({ id })),
+      map((res) => res.data.launch),
+      finalize(() => this.loading = false)
+  );
 
   ngOnInit(): void {
   }
